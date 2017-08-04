b3dba6f4632f234a52ee940e44b8b69c48486dc3
toatzin Padilla <toatzin_2@hotmail.com>
2017-08-04 16:02:36 -0500

    Excercise 1.1

--- Cracking Code/Arrays and Strings/1.1.js
+++ Cracking Code/Arrays and Strings/1.1.js
@@ -2,6 +2,7 @@
 // What if you cannot use additional data structures?
 
 function isUnique_hash(str){
+    if(str.length > 128) return false;
     str = str.trim();
     var hashMap = {};
     for (var i = 0; i < str.length; i++) {
@@ -15,6 +16,7 @@ function isUnique_hash(str){
 }
 
 function isUnique(str){
+    if(str.length > 128) return false;
     str = str.trim();
     var arr = str.split('').sort();
     var tmp = "";
@@ -23,6 +25,19 @@ function isUnique(str){
             return false;
         }
         tmp = arr[i];
+    }
+    return true;
+}
+
+function isUnique_array(str){
+    if(str.length > 128) return false;
+    str = str.trim();
+    var arr = [];
+    for (var i = 0; i < str.length; i++) {
+        if(arr[str[i]]){
+            return false;
+        }
+        arr[str[i]] = true;
     }
     return true;
 }
@@ -36,7 +51,12 @@ console.log("NO DATA STTRUCTURE'" + str2 + "': " + isUnique(str2));
 console.log("NO DATA STTRUCTURE'" + str3 + "': " + isUnique(str3));
 console.log("NO DATA STTRUCTURE'" + str4 + "': " + isUnique(str4));
 
-console.log("DATA STTRUCTURE'" + str1 + "': " + isUnique_hash(str1));
-console.log("DATA STTRUCTURE'" + str2 + "': " + isUnique_hash(str2));
-console.log("DATA STTRUCTURE'" + str3 + "': " + isUnique_hash(str3));
-console.log("DATA STTRUCTURE'" + str4 + "': " + isUnique_hash(str4));
+console.log("DATA STTRUCTURE HASH'" + str1 + "': " + isUnique_hash(str1));
+console.log("DATA STTRUCTURE HASH'" + str2 + "': " + isUnique_hash(str2));
+console.log("DATA STTRUCTURE HASH'" + str3 + "': " + isUnique_hash(str3));
+console.log("DATA STTRUCTURE HASH'" + str4 + "': " + isUnique_hash(str4));
+
+console.log("DATA STTRUCTURE ARRAY'" + str1 + "': " + isUnique_array(str1));
+console.log("DATA STTRUCTURE ARRAY'" + str2 + "': " + isUnique_array(str2));
+console.log("DATA STTRUCTURE ARRAY'" + str3 + "': " + isUnique_array(str3));
+console.log("DATA STTRUCTURE ARRAY'" + str4 + "': " + isUnique_array(str4));
